7e1fae92c66ba5869deab6c62286bd96ca3f73b5
Forhadul Islam <forhadul.islam@nokia.com>
2020-02-17 11:40:30 +0200

    updated Examples

---
+++ 08.Functions/add.rs
@@ -0,0 +1,4 @@
+
+pub fn add_ints(){
+    println!("Add integers")
+}
--- 08.Functions/example.rs
+++ 08.Functions/example.rs
@@ -1,6 +1,18 @@
+mod add;
+use add::add_ints;
+use remove::remove_ints;
+
+mod remove {
+    pub fn remove_ints(){
+        println!("Remove integers")
+    }
+}
+
+
 fn main() {
     // Statements here are executed when the compiled binary is called
 
     // Print text to the console
-    println!("Hello World!");
+    add_ints();
+    remove_ints();
 }
