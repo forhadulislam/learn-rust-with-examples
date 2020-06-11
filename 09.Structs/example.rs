0678da0b5d592f6aad67fe2f795e34aa92a26937
Sadi <ronex.sadi@yahoo.com>
2020-06-12 02:22:02 +0300

    updated structs example

--- 09.Structs/example.rs
+++ 09.Structs/example.rs
@@ -7,19 +7,27 @@ impl User{
     fn get_user_as_string(&self) -> String {
         format!("name: {}, email: {}", self.name, self.email)
     }
+
+    fn update_email(&mut self, email: String){
+        self.email = email
+    }
 }
 
 fn main() {
     // Statements here are executed when the compiled binary is called
 
 
-    let user = User{
+    let mut user = User{
         name: "John Doe".to_string(),
         email: "john.doe@email.com".to_string(),
     };
 
     println!("New User name: {}", user.name);
     println!("New User email: {}", user.email);
+
+    println!("get_user_as_string: {}", user.get_user_as_string());
+
+    user.update_email("ejohn.parker@email.com".to_string());
 
     println!("get_user_as_string: {}", user.get_user_as_string());
 }
